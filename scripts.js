9e94d5f66457a322d3ae78d2713ce16df5269395
RavTuazon <rtuazon999@gmail.com>
2025-05-02 11:52:19 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Basic interactivity (e.g., form submission alert)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+    this.reset();
+});
